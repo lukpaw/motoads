759b6c1c4f6f717d91f9e81a50becf6b704ae8ec
lukpaw <lukpaw@gmail.com>
2013-11-04 22:37:57 +0100

    Karma conf for test.

---
+++ config/karma.conf.js
@@ -0,0 +1,34 @@
+module.exports = function(config){
+    config.set({
+    basePath : '../',
+
+    files : [
+      'app/lib/angular/angular.js',
+      'app/lib/angular/angular-*.js',
+      'app/lib/ui-bootstrap/ui-bootstrap-*.js',
+      'app/lib/angular/angular-mocks.js',
+      'app/js/**/*.js',
+      'test/unit/**/*.js'
+    ],
+
+    exclude: ['app/lib/angular/angular-scenario.js'],
+
+    autoWatch : true,
+
+    frameworks: ['jasmine'],
+
+    browsers : ['Chrome'],
+
+    plugins : [
+      'karma-junit-reporter',
+      'karma-chrome-launcher',
+      'karma-firefox-launcher',
+      'karma-jasmine'
+    ],
+
+    junitReporter : {
+      outputFile: 'test_out/unit.xml',
+      suite: 'unit'
+    }
+
+});};
