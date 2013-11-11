7f0f01b8e40c8c3ef8886911c5b608b38bd741e7
lukpaw <lukpaw@gmail.com>
2013-11-11 18:01:03 +0100

    Mongodb

--- server/routes/adverts.js
+++ server/routes/adverts.js
@@ -1,4 +1,34 @@
+var mongo = require('mongodb');
+
+var Server = mongo.Server;
+var Db = mongo.Db;
+var BSON = mongo.BSONPure;
+
+var server = new Server('localhost', 27017, {auto_reconnect: true});
+db = new Db('motoads', server);
+
+db.open(function(err, db) {
+  if (!err) {
+    console.log("Connected to 'motoads' database");
+    db.collection('adverts', {strict: true}, function(err, collection) {
+      if (err) {
+        console.log("The 'adverts' collection doesn't exist. Creating it with sample data...");
+        populateDB();
+      }
+    });
+  }
+});
+
 exports.findAll = function(req, res) {
+  db.collection('adverts', function(err, collection) {
+    collection.find().toArray(function(err, items) {
+      console.log('adverts send from DB');
+      res.send(items);
+    });
+  });
+};
+
+var populateDB = function() {
   var fs = require('fs');
   var file = './server/data/adverts.json';
 
@@ -6,6 +36,16 @@ exports.findAll = function(req, res) {
     if (err) {
       throw err;
     }
-    res.send(JSON.parse(data));
+    var adverts = JSON.parse(data);
+    db.collection('adverts', function(err, collection) {
+      if (err) {
+        throw err;
+      }
+      collection.insert(adverts, {safe: true}, function(err, result) {
+        if (err) {
+          throw err;
+        }
+      });
+    });
   });
 };
--- server/routes/countries.js
+++ server/routes/countries.js
@@ -1,4 +1,34 @@
+var mongo = require('mongodb');
+
+var Server = mongo.Server;
+var Db = mongo.Db;
+var BSON = mongo.BSONPure;
+
+var server = new Server('localhost', 27017, {auto_reconnect: true});
+db = new Db('motoads', server);
+
+db.open(function(err, db) {
+  if (!err) {
+    console.log("Connected to 'motoads' database");
+    db.collection('countries', {strict: true}, function(err, collection) {
+      if (err) {
+        console.log("The 'countries' collection doesn't exist. Creating it with sample data...");
+        populateDB();
+      }
+    });
+  }
+});
+
 exports.findAll = function(req, res) {
+  db.collection('countries', function(err, collection) {
+    collection.find().toArray(function(err, items) {
+      console.log('countries send from DB');
+      res.send(items);
+    });
+  });
+};
+
+var populateDB = function() {
   var fs = require('fs');
   var file = './server/data/countries.json';
 
@@ -6,7 +36,16 @@ exports.findAll = function(req, res) {
     if (err) {
       throw err;
     }
-    res.send(JSON.parse(data));
+    var countries = JSON.parse(data);
+    db.collection('countries', function(err, collection) {
+      if (err) {
+        throw err;
+      }
+      collection.insert(countries, {safe: true}, function(err, result) {
+        if (err) {
+          throw err;
+        }
+      });
+    });
   });
 };
-
